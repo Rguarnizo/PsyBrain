2d5d699cccb1af161341ca5e0315653cf0a33658
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2020-10-17 17:38:17 -0500

    Encapsulando SignIn Page

--- lib/singin_screen.dart
+++ lib/singin_screen.dart
@@ -1,6 +1,5 @@
 import 'package:PsyBrain/user_chat.dart';
 import 'package:flutter/material.dart';
-
 import 'login_buttons.dart';
 
 class SignInScreen extends StatelessWidget {
@@ -14,99 +13,31 @@ class SignInScreen extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              '¡Bienvenido de nuevo!',
-              style: TextStyle(
-                  color: Colors.black,
-                  fontFamily: 'SourceSansPro',
-                  fontSize: 28.2,
-                  fontWeight: FontWeight.w400
-                  //fontWeight: FontStyle.italic,
-                  ),
-            ),
-            Text(
-              'Ingresa para continuar',
-              style: TextStyle(
-                  color: Colors.grey,
-                  //fontWeight: FontWeight.w700,
-                  fontFamily: 'SourceSansPro',
-                  fontSize: 18.2,
-                  fontWeight: FontWeight.w300),
-            ),
-            Container(
-                margin: EdgeInsets.only(
-                    left: 30.0, right: 30.0, top: 60.5, bottom: 30.5),
-                child: Image.asset('assets/imgs/login_img.png')),
+            _titulo(),
+            _subtitle(),
+            _image(),
             Container(
               height: 108.0,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  TextField(
-                    cursorColor: Color(0xFFf1e4e8),
-                    decoration: InputDecoration(
-                        suffixIcon: Icon(Icons.alternate_email),
-                        hintText: 'Ingresa aqui tu email',
-                        contentPadding: EdgeInsets.only(left: 14.0),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide:
-                              BorderSide(color: Color(0xFFceb1be), width: 2.0),
-                          borderRadius: BorderRadius.circular(9.0),
-                        ),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(9))),
-                  ),
-                  TextField(
-                    cursorColor: Color(0xFFf1e4e8),
-                    obscureText: true,
-                    decoration: InputDecoration(
-                        suffixIcon: Icon(Icons.face),
-                        hintText: 'Ingresa aqui tu contraseña ',
-                        contentPadding: EdgeInsets.only(left: 14.0),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide:
-                              BorderSide(color: Color(0xFFceb1be), width: 2.0),
-                          borderRadius: BorderRadius.circular(9.0),
-                        ),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(9))),
-                  ),
+                  _emailInput(),
+                  _passwordInput(),
                 ],
               ),
             ),
             Flexible(
               child: Container(
                 margin: EdgeInsets.only(top: 25),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    MyButton(
-                        buttonName: 'Entrar',
-                        gradientColors: [Color(0xFFceb1be)],
-                        textColor: Colors.white,
-                        width: 115,
-                        withShadow: true,
-                        action: () {
-                          print('Hola');
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => UserChat()));
-                        }),
-                    MyButton(
-                      gradientColors: [Color(0xFFceb1be)],
-                      withShadow: false,
-                      width: 50,
-                      image: AssetImage('assets/imgs/google_icon.png'),
-                    ),
-                    MyButton(
-                      buttonName: 'Crea tu cuenta',
-                      gradientColors: [Color(0xFFf1e4e8)],
-                      textColor: Color(0xFFCEB1BE),
-                      width: 155,
-                      withShadow: false,
-                    ),
-                  ],
+                child: Expanded(
+                    child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      botonEntrar(context),
+                      botonGoogle(),
+                      botonCrearCuenta(),                                  
+                    ],
+                  ),
                 ),
               ),
             ),
@@ -115,4 +46,114 @@ class SignInScreen extends StatelessWidget {
       ),
     );
   }
+
+  Widget _titulo() {
+
+    return Text(
+      '¡Bienvenido de nuevo!',
+      style: TextStyle(
+          color: Colors.black,
+          fontFamily: 'SourceSansPro',
+          fontSize: 28.2,
+          fontWeight: FontWeight.w400
+          //fontWeight: FontStyle.italic,
+          ),
+    );
+  }
+
+  Widget _subtitle() {
+    return Text(
+      'Ingresa para continuar',
+      style: TextStyle(
+          color: Colors.grey,
+          //fontWeight: FontWeight.w700,
+          fontFamily: 'SourceSansPro',
+          fontSize: 18.2,
+          fontWeight: FontWeight.w300),
+    );
+  }
+
+  Widget _image() {
+    return Container(
+      margin: EdgeInsets.only(
+          left: 30.0, right: 30.0, top: 60.5, bottom: 30.5),
+      child: Image.asset('assets/imgs/login_img.png'));
+  }
+
+  Widget _emailInput() {
+
+    return TextField(
+      cursorColor: Color(0xFFf1e4e8),
+      decoration: InputDecoration(
+          suffixIcon: Icon(Icons.alternate_email),
+          hintText: 'Ingresa aqui tu email',
+          contentPadding: EdgeInsets.only(left: 14.0),
+          focusedBorder: OutlineInputBorder(
+            borderSide:
+                BorderSide(color: Color(0xFFceb1be), width: 2.0),
+            borderRadius: BorderRadius.circular(9.0),
+          ),
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(9))),
+    );
+  }
+
+  Widget _passwordInput() {
+    return TextField(
+      cursorColor: Color(0xFFf1e4e8),
+      obscureText: true,
+      decoration: InputDecoration(
+          suffixIcon: Icon(Icons.face),
+          hintText: 'Ingresa aqui tu contraseña ',
+          contentPadding: EdgeInsets.only(left: 14.0),
+          focusedBorder: OutlineInputBorder(
+            borderSide:
+                BorderSide(color: Color(0xFFceb1be), width: 2.0),
+            borderRadius: BorderRadius.circular(9.0),
+          ),
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(9))),
+    );
+  }
+
+    Widget botonEntrar(BuildContext context) {
+    return MyButton(
+      buttonName: 'Entrar',
+      gradientColors: [Color(0xFFceb1be)],
+      textColor: Colors.white,
+      width: 110,
+      withShadow: true,
+      action: () {
+        print('Hola');
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => UserChat()));
+      });
+  }
+  
+
+  Widget botonGoogle(){
+    return MyButton(
+        gradientColors: [Color(0xFFceb1be)],
+        withShadow: false,
+        width: 50,
+        image: AssetImage('assets/imgs/google_icon.png'),
+      );
+  }
+
+    Widget botonCrearCuenta() {
+      return MyButton(
+        action: () {},
+        buttonName: 'Crea tu cuenta',
+        gradientColors: [Color(0xFFf1e4e8)],
+        textColor: Color(0xFFCEB1BE),
+        width: 150,
+        withShadow: false,
+    );
+  }
 }
+
+  
+
+
