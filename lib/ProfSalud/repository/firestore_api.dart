182623406b37d8903095d12153fbd903743d4fcc
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2020-12-13 20:26:22 -0500

    FireStore Api Documentation.

--- lib/ProfSalud/repository/firestore_api.dart
+++ lib/ProfSalud/repository/firestore_api.dart
@@ -9,6 +9,7 @@ class FireStoreApi {
   final String PROFESIONALSALUD = 'ProfesionalSalud';
   final String CHATS = "Chats";
 
+  /// Crea un usuario con el email respectivo y la contraseña respectiva usando la Logica de Firebase Authentication.
   Future<User> crearProfSalud(String email, String contrasena) async {
     UserCredential userRef = await _apiFireAuth.createUserWithEmailAndPassword(
         email: email, password: contrasena);
@@ -16,6 +17,8 @@ class FireStoreApi {
     return userRef.user;
   }
 
+  /// Guarda la información de un usuario con el id especificado y la instancia de un Profesional de la salud, mediante los metodos de
+  /// FireStore.
   Future<void> guardarInformacion(ProfSalud profSalud, String uid) {
     profSalud.id = uid;
     profSalud.imageURL = _apiFireAuth.currentUser.photoURL ??
@@ -26,10 +29,13 @@ class FireStoreApi {
         .set(profSalud.json());
   }
 
+  /// Obtiene la información de un usuario Profesional de la salud concreto con el uid especificado, usando los metodos de 
+  /// FireStore.
   Future<DocumentSnapshot> obtenerInformacion(String uid) {
     return _apiFireStore.collection(PROFESIONALSALUD).doc(uid).get();
   }
 
+  /// Retorna un Stream de los chats de un Usuario especificado en el uid.
   Stream<QuerySnapshot> getChats(String uid) {
     return _apiFireStore
         .collection('Chats')
@@ -38,10 +44,12 @@ class FireStoreApi {
         .snapshots();
   }
 
+  /// Actualiza la información de un Usuario especificado por el uid y el Json correspondiente.
   Future<void> actulizarData(Map<String, dynamic> data, String uid) {
     return _apiFireStore.collection(PROFESIONALSALUD).doc(uid).update(data);
   }
 
+  /// Obtiene la lista completa de usuarios con los cuales su nombre empiece por la query descrita.
   Stream<QuerySnapshot> getListUsers(String query) {
     String limit;
     if (query != '') {
@@ -60,6 +68,7 @@ class FireStoreApi {
         return users;
   }
 
+  ///Obtiene los usuarios Profesional de la salud los cuales empicen con el nombre tal cual la query.
   Stream<QuerySnapshot> getListHealts(String query) {
     String limit;
     if (query != '') {
@@ -73,11 +82,12 @@ class FireStoreApi {
         .where('Nombres', isGreaterThanOrEqualTo: query)
         .where('Nombres', isLessThan: limit)
         .snapshots();
-    
-
+  
         return healts;
   }
 
+  /// Inicia el chat con los usuarios que se especifiquen los dos uids y con el mensaje especificado, guarda un timestamp 
+  /// actual y los dos uids de los usuarios.
   Future<void> iniciarChat(
       String anotherUserUid, String uid, String message) async {
     await _apiFireStore.collection('Chats').doc('$uid-$anotherUserUid').set({
@@ -98,6 +108,7 @@ class FireStoreApi {
     });
   }
 
+  /// Obtiene la información de todos los chats que tenga el usuario con el uid correspondiente.
   Future<List<String>> getChatsPS(String currentUserID) async {
     List<String> chatRecount = [];
     var totalChatSnap = await _apiFireStore.collection(CHATS).get();
@@ -129,10 +140,12 @@ class FireStoreApi {
     return chatRecopilationString;
   }
 
+  /// Revisa si existe el chat con el UID especificado. True si existe, false si no.
   Future<bool> chatExist(String chatID)async {
       return await _apiFireStore.collection('Chats').doc(chatID).get().then((value) => value.exists);
   }
 
+  /// Guarda la calificación hacia un Profesional de la salud en un array de calificaciones.
   Future<void> calificarProfSalud(String uid, double calificacion) async{
     
     List data = (await obtenerInformacion(uid)).data()['Calificaciones'];
